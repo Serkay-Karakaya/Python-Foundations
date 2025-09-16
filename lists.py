0b248ff0428b90f974bac5d1fce39e34f85e5369
SerkayKarakaya <serkay1karakaya@gmail.com>
2025-09-16 10:20:20 +0300

    ...

---
+++ lists.py
@@ -0,0 +1,16 @@
+"""
+message = " Hello, My name is Serkay Karakaya".split()
+print(message)
+print(message[4],message[5]) # Serkay Karakaya yazar
+"""
+"""
+my_list = ["seko",2,True,4.6]
+print(my_list)
+"""
+"""
+list1 = ["one","two"]
+list2 = ["three","four"]
+numbers = list1 + list2 #tek liste halinde yazar
+print(numbers)
+"""
+#-------------------------------------------------
