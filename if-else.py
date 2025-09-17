a2befae1105b2ca35eaa4eadcf110d3bf3e53018
SerkayKarakaya <serkay1karakaya@gmail.com>
2025-09-17 16:54:50 +0300

    ...

---
+++ if-else.py
@@ -0,0 +1,15 @@
+"""
+if int(input("Sayı Gir: "))>2:
+    print("Büyük")
+else :
+    print("Küçük")
+"""
+"""
+if input("username: ") == "seko" :
+     if input("pass: ") == 123:
+        print("Hoşgeldin")
+     else: 
+          print("Password yanlış")
+else:
+        print("Username yanlış")
+"""
