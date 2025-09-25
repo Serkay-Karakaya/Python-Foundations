dce82a7fb7a10ee1c85095826018e3f1f78aaad5
SerkayKarakaya <serkay1karakaya@gmail.com>
2025-09-25 16:29:07 +0300

    ....

---
+++ functions.py
@@ -0,0 +1,17 @@
+""" def sayHello(name):
+     print("Hello " + name)
+sayHello(input("Name: "))
+"""
+#-------------------------------------
+""" def total(num1,num2):
+     return num1 + num2
+num1 = int(input("1.Sayı: "))
+num2 = int(input("2.Sayı: "))
+print(total(num1,num2))
+"""
+#-------------------------------------
+def repeat(n,i):
+    print(n * i)
+n = input("kelime: ")
+i = int(input("Kaç Kere: "))
+repeat(n,i)
